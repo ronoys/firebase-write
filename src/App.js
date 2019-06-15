cbc1321212ead290bf8c3f1125639960d712d37c
ronoysarkar <ronoysarkar.mail@gmail.com>
2019-06-15 19:06:26 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Nav from 'react-bootstrap/Nav';
+
+
 function App() {
   return (
     <div className="App">
+
+      <Nav
+        activeKey="/home"
+        onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+        variant = "pills"
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled  
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+
       <header className="App-header">
+        
+        
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a paragraph
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      
     </div>
   );
 }
