4016c3dbf1514eaab7ccfb56c94f6691ff890025
ronoysarkar <ronoysarkar.mail@gmail.com>
2019-06-15 20:47:04 -0400

    v2

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import Nav from 'react-bootstrap/Nav';
 
 
-function App() {
-  return (
-    <div className="App">
 
-      <Nav
-        activeKey="/home"
-        onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-        variant = "pills"
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled  
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
 
-      <header className="App-header">
-        
-        
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a paragraph
-        </p>
-        
-      </header>
-
-      
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Getch</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import Users from './users'
+import Contact from './contact'
+const routing = (
+  <Router>
+    <div class = "textfield">
+      <div class = "tab">
+      <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+      </ul>
+      </div>
+      <Route exact path="/" component={App} />
+      <Route path="/users" component={Users} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
